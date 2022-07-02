5b282237ecdb0e56e9610acf1f0bda80487d10cf
Caju <nathandebaz266@gmail.com>
2022-07-02 02:31:14 +0200

    Upgrading to heroku-22 + signature

--- app/javascript/components/burger.js
+++ app/javascript/components/burger.js
@@ -1,7 +1,5 @@
 export const startBurger = () => {
-  console.log("burger loaded");
   document.querySelector(".navbar__burger__button").addEventListener("click", () => {
-    console.log("burger clicked");
     document.getElementById("sidebar").classList.toggle("hidden");
     document.getElementById("filter").classList.toggle("hidden");
   });
